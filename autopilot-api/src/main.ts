7c097583b35c45b67896b91921a84a1f9efa24a8
Kangwa <chimfwembe.kangwa@mlfafrica.org>
2026-06-14 00:51:54 +0200

    Refactor CORS configuration in main.ts
    - Simplified CORS settings by enabling CORS without specifying origins, methods, or headers.
    - This change enhances flexibility in API access while maintaining default security settings.

--- autopilot-api/src/main.ts
+++ autopilot-api/src/main.ts
@@ -39,12 +39,7 @@ async function bootstrap() {
         'https://mako.tekreminnovations.com',
       ];
 
-  app.enableCors({
-    origin: '*',
-    // methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-    // allowedHeaders: ['Content-Type', 'Authorization'],
-    // credentials: true,
-  });
+  app.enableCors();
 
   app.useStaticAssets(join(process.cwd(), 'public'));
 
